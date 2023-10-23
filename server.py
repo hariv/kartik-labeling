34954695d615959f9a44a4df3e3564317e3d40ec
hariv <hvenugopalan@ucdavis.edu>
2023-10-22 17:44:44 -0700

    debug

--- server.py
+++ server.py
@@ -94,6 +94,9 @@ class StaticServer(BaseHTTPRequestHandler):
                 return self.read_file(image_file, binary=True)
             
     def fetch_static_content(self, path, version, user, request_count):
+        print(version)
+        print(request_count)
+        print(user)
         if version in self.versions_list and user in self.users_list:
             resource_file = os.path.join(self.assets_path, path[1:])
             _, resource_extension = os.path.splitext(resource_file)
